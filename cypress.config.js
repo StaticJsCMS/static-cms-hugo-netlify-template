d8ac32db202090e3723c35178ef149caccba8bb6
Daniel Lautzenheiser <lautzd@gmail.com>
2023-01-04 10:37:08 -0500

    Remove cypress config

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  projectId: "smvz27",
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    supportFile: false
-  },
-});
